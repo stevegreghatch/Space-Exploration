35bbd096af5620183d2b5ac6ec27ee751195ced3
steve <stevegreghatch@gmail.com>
2024-07-18 12:48:46 -0400

    created test endpoint

--- app.py
+++ app.py
@@ -125,6 +125,11 @@ async def get_astronauts_by_mission(mission: str):
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
+@app.get('/space-exploration/test')
+async def get_test_response():
+    return True
+
+
 def main():
     logger.info('Logger configured')
     logger.info('Starting FastAPI application')
